77ee4989d52a02fb5f5df37811018f783e549832
Vino aryo <vinoaryo2000@gmail.com>
2022-08-09 13:42:29 +0700

    Adding custom theme button

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const changeTheme = () => {
   const changeThemeLimit = 5;
   const remainingThemes = changeThemeLimit - themeCounter;
   const themeChanger = document.getElementById("themeChanger");
+  const green = document.getElementById("green");
+  const red = document.getElementById("red");
   let info = document.getElementById("info");
   let home = document.getElementById("home");
 
@@ -30,6 +32,8 @@ const changeTheme = () => {
     info.style.color = "red";
     info.textContent = `You have reached the theme limit of ${changeThemeLimit} times.`;
     themeChanger.style.display = "none";
+    green.style.display = "none";
+    red.style.display = "none";
     home.textContent = "Restart!";
   } else {
     console.log("themeCounter: ", themeCounter);
@@ -41,7 +45,11 @@ const changeTheme = () => {
 const greenMode = () => {
   const theme = document.querySelector("body");
   const themeChanger = document.getElementById("themeChanger");
+  const changeThemeLimit = 5;
+  const remainingThemes = changeThemeLimit - themeCounter;
   const green = document.getElementById("green");
+  const red = document.getElementById("red");
+  let info = document.getElementById("info");
   theme.classList.toggle("greenTheme");
 
   if (green.innerHTML === "Green") {
@@ -51,12 +59,32 @@ const greenMode = () => {
     green.innerHTML = "Green";
     themeChanger.innerHTML = "Day";
   }
+
+  themeCounter += 1;
+  if (themeCounter >= changeThemeLimit) {
+    theme.classList.remove("greenThemes");
+    console.log("Theme limit reached");
+    info.style.color = "red";
+    info.textContent = `You have reached the theme limit of ${changeThemeLimit} times.`;
+    themeChanger.style.display = "none";
+    green.style.display = "none";
+    red.style.display = "none";
+    home.textContent = "Restart!";
+  } else {
+    console.log("themeCounter: ", themeCounter);
+    console.log("remainingThemes: ", remainingThemes);
+    info.textContent = `You have ${remainingThemes} themes remaining.`;
+  }
 };
 
 const redMode = () => {
   const theme = document.querySelector("body");
   const themeChanger = document.getElementById("themeChanger");
+  const changeThemeLimit = 5;
+  const remainingThemes = changeThemeLimit - themeCounter;
+  const green = document.getElementById("green");
   const red = document.getElementById("red");
+  let info = document.getElementById("info");
   theme.classList.toggle("redTheme");
 
   if (red.innerHTML === "Red") {
@@ -66,4 +94,37 @@ const redMode = () => {
     red.innerHTML = "Red";
     themeChanger.innerHTML = "Day";
   }
+
+  themeCounter += 1;
+  if (themeCounter >= changeThemeLimit) {
+    theme.classList.remove("redTheme");
+    console.log("Theme limit reached");
+    info.style.color = "red";
+    info.textContent = `You have reached the theme limit of ${changeThemeLimit} times.`;
+    themeChanger.style.display = "none";
+    green.style.display = "none";
+    red.style.display = "none";
+    home.textContent = "Restart!";
+  } else {
+    console.log("themeCounter: ", themeCounter);
+    console.log("remainingThemes: ", remainingThemes);
+    info.textContent = `You have ${remainingThemes} themes remaining.`;
+  }
 };
+
+const customTheme = () => {
+  // Make the backgrund and text color the same as the input given by user
+  const theme = document.querySelector("body");
+  const themeChanger = document.getElementById("themeChanger");
+  const changeThemeLimit = 5;
+  const remainingThemes = changeThemeLimit - themeCounter;
+  const customButton = document.getElementById("customTheme");
+  const customBg = document.getElementById("customBg");
+  const customText = document.getElementById("customText");
+
+  customButton.style.color = 'white';
+
+  theme.style.backgroundColor = customBg.value;
+  theme.style.color = customText.value;
+
+}
